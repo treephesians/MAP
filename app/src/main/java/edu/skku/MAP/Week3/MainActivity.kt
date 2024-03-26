d84b620a9b672f83d4a271757194f871276ebe44
ephesi4ns <ephesi4ns623@gmail.com>
2024-03-26 20:25:20 +0900

    Week4

--- app/src/main/java/edu/skku/MAP/Week3/MainActivity.kt
+++ app/src/main/java/edu/skku/MAP/Week3/MainActivity.kt
@@ -1,6 +1,11 @@
 package edu.skku.MAP.Week3
 
+import android.content.Context
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -8,6 +13,9 @@ import androidx.core.view.WindowInsetsCompat
 import android.widget.TextView
 import android.widget.ImageView
 import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.ListAdapter
+import android.widget.ListView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
@@ -21,60 +29,79 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-        var counter = 0
-        var textview = findViewById<TextView>(R.id.textView)
-        var imageView = findViewById<ImageView>(R.id.imageView)
-        var left_btn = findViewById<Button>(R.id.button)
-        var right_btn = findViewById<Button>(R.id.button2)
+        var linearLayout = findViewById<LinearLayout>(R.id.sub_layout)
+        var btn = findViewById<Button>(R.id.button)
 
-        textview.text = "pizza"
-        imageView.setImageResource(R.drawable.pizza)
+        btn.setOnClickListener {
+            val layoutInflater: LayoutInflater =
+                applicationContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as
+                        LayoutInflater
+            layoutInflater.inflate(R.layout.sub_layout, linearLayout, true)
 
-        right_btn.setOnClickListener{
-            counter += 1
-            when(counter){
-                1 -> {
-                    textview.text = "chicken"
-                    imageView.setImageResource(R.drawable.chicken)
-                }
-                2 -> {
-                    textview.text = "hamburger"
-                    imageView.setImageResource(R.drawable.hamburger)
-                }
-                3 -> {
-                    textview.text = "ramen"
-                    imageView.setImageResource(R.drawable.ramen)
-                }
-                4 -> {
-                    Toast.makeText(this@MainActivity, "Last Image", Toast.LENGTH_LONG).show()
-                    counter -= 1
-                }
-            }
+            var img1 = findViewById<ImageView>(R.id.imageView)
+            var img2 = findViewById<ImageView>(R.id.imageView2)
+
+            img1.setImageResource(R.drawable.bbq)
+            img2.setImageResource(R.drawable.bhc)
         }
 
+        var pizza_btn = findViewById<Button>(R.id.button1)
+        var hamburger_btn = findViewById<Button>(R.id.button2)
+        var chicken_btn = findViewById<Button>(R.id.button3)
+        var items = arrayListOf<Restaurant>()
+        var mainList = findViewById<ListView>(R.id.listview)
 
-
-        left_btn.setOnClickListener{
-            counter -= 1
-            when(counter){
-                0 -> {
-                    textview.text = "pizza"
-                    imageView.setImageResource(R.drawable.pizza)
-                }
-                1 -> {
-                    textview.text = "chicken"
-                    imageView.setImageResource(R.drawable.chicken)
-                }
-                2 -> {
-                    textview.text = "hamburger"
-                    imageView.setImageResource(R.drawable.hamburger)
-                }
-                -1 -> {
-                    Toast.makeText(this@MainActivity, "First Image", Toast.LENGTH_LONG).show()
-                    counter += 1
-                }
-            }
-
+        pizza_btn.setOnClickListener{
+            items = arrayListOf<Restaurant>()
+            items.add(Restaurant(R.drawable.domino, "domino"))
+            items.add(Restaurant(R.drawable.pizzahut, "pizzahut"))
+            items.add(Restaurant(R.drawable.pizzanarachickengongju, "pizzanarachickengongju"))
+            val listAdaptor = CustomAdapter(this, items)
+            mainList.adapter = listAdaptor
         }
+
+        hamburger_btn.setOnClickListener{
+            items = arrayListOf<Restaurant>()
+            items.add(Restaurant(R.drawable.burgerking, "burgerking"))
+            items.add(Restaurant(R.drawable.lotteria, "lotteria"))
+            items.add(Restaurant(R.drawable.mcdonalds, "mcdonalds"))
+            items.add(Restaurant(R.drawable.momstouch, "momstouch"))
+            val listAdaptor = CustomAdapter(this, items)
+            mainList.adapter = listAdaptor
+        }
+
+        chicken_btn.setOnClickListener{
+            items = arrayListOf<Restaurant>()
+            items.add(Restaurant(R.drawable.bbq, "bbq"))
+            items.add(Restaurant(R.drawable.bhc, "bhc"))
+            items.add(Restaurant(R.drawable.goobne, "goobne"))
+            items.add(Restaurant(R.drawable.pizzanarachickengongju, "pizzanarachickengongju"))
+            val listAdaptor = CustomAdapter(this, items)
+            mainList.adapter = listAdaptor
+        }
+
+    }
+}
+class Restaurant (val id: Int, val name: String)
+class CustomAdapter(val context: Context, val items: ArrayList<Restaurant>): BaseAdapter(){
+    override fun getCount():Int {
+        return items.size
+    }
+    override fun getItem(position: Int): Any{
+        return items.get(position)
+    }
+    override fun getItemId(position: Int) : Long{
+        return 0
+    }
+    override fun getView(i:Int, cvtView: View?, parent: ViewGroup?):View{
+        val inflater: LayoutInflater = LayoutInflater.from(context)
+        var view: View = inflater.inflate(R.layout.create_item, null)
+        var imgView = view.findViewById<ImageView>(R.id.imageView7)
+        var textView = view.findViewById<TextView>(R.id.textView3)
+        textView.setText(items.get(i).name)
+        imgView.setImageResource(items.get(i).id)
+
+        return view
+
     }
 }
